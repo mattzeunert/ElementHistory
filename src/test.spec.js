cacc0976d8f35218cde8f6d60abc89d6ad7c062b
Matt Zeunert <matt@mostlystatic.com>
2017-08-29 22:56:57 +0800

    Track el.className assignments

--- src/test.spec.js
+++ src/test.spec.js
@@ -1,9 +1,12 @@
+function getLastHistoryItem(el, historyKey){
+  return el.__elementHistory[historyKey][0]
+}
+
 describe("A suite", function() {
-  it("contains spec with an expectation", function specFunctionName() {
+  it("It tracks setAttribute calls", function specFunctionName() {
     var el = document.createElement('div')
     el.setAttribute('style', 'background: red')
-    var styleHistory = el.__elementHistory.style
-    var lastHistoryItem = styleHistory[0]
+    var lastHistoryItem = getLastHistoryItem(el, 'style')
     expect(lastHistoryItem.actionType).toEqual('setAttribute')
     expect(lastHistoryItem.actionArguments).toEqual(['style', 'background: red'])
     expect(lastHistoryItem.oldValue).toEqual(null)
@@ -11,4 +14,15 @@ describe("A suite", function() {
     expect(lastHistoryItem.callstack).toContain('specFunctionName')
     expect(lastHistoryItem.date).not.toBe(undefined)
   });
+  it("Tracks className assignemnts", function(){
+    var el = document.createElement('div')
+    el.className += 'cake'
+    var lastHistoryItem = getLastHistoryItem(el, 'className')
+    expect(lastHistoryItem.actionType).toEqual('className assignment')
+    expect(lastHistoryItem.actionArguments).toEqual(['cake'])
+    expect(lastHistoryItem.oldValue).toEqual('')
+    expect(lastHistoryItem.newValue).toEqual('cake')
+  })
+
+  // todo: make sure classlist and .setAttribute(class) are also tracked under className
 });
--- src/trackHistory.js
+++ src/trackHistory.js
@@ -13,31 +13,68 @@ var trackingEventTypes = [
                 actionArguments: [attrName, attrValue]
             }
         }
+    },
+    {
+        obj: Element.prototype,
+        key: 'className',
+        getValue: function(){
+            return this.className
+        },
+        getActionInfo: function(newValue){
+            return {
+                actionType: 'className assignment',
+                historyKey: 'className',
+                actionArguments: [newValue]
+            }
+        }
     }
 ]
 
 function enableTracking(){
     console.log("enable")
     trackingEventTypes.forEach(function(trackingEventType){
-        trackingEventType.originalFunction = trackingEventType.obj[trackingEventType.fnName];
-        trackingEventType.obj[trackingEventType.fnName] = function(){
-            var before = trackingEventType.getValue.apply(this, arguments)
-            var actionInfo = trackingEventType.getActionInfo.apply(this, arguments)
-
-            var ret = trackingEventType.originalFunction.apply(this, arguments)
-            var after = trackingEventType.getValue.apply(this, arguments)
-
-            addHistoryItem(this, actionInfo.historyKey, {
-                actionType: actionInfo.actionType,
-                actionArguments: actionInfo.actionArguments,
-                oldValue: before,
-                newValue: after,
-                callstack: Error().stack,
-                date: new Date()
-            })
-
-            return ret
+        if (trackingEventType.obj && trackingEventType.fnName) {
+            trackingEventType.originalFunction = trackingEventType.obj[trackingEventType.fnName];
+            trackingEventType.obj[trackingEventType.fnName] = function(){
+                var before = trackingEventType.getValue.apply(this, arguments)
+                var actionInfo = trackingEventType.getActionInfo.apply(this, arguments)
+    
+                var ret = trackingEventType.originalFunction.apply(this, arguments)
+                var after = trackingEventType.getValue.apply(this, arguments)
+    
+                addHistoryItem(this, actionInfo.historyKey, {
+                    actionType: actionInfo.actionType,
+                    actionArguments: actionInfo.actionArguments,
+                    oldValue: before,
+                    newValue: after
+                })
+    
+                return ret
+            }
         }
+        if (trackingEventType.obj && trackingEventType.key) {
+            var originalDescriptor = Object.getOwnPropertyDescriptor(trackingEventType.obj, trackingEventType.key)
+            Object.defineProperty(trackingEventType.obj, trackingEventType.key, {
+                get: function(){
+                    return originalDescriptor.get.apply(this, arguments)
+                },
+                set: function(){
+                    console.log("set classname")
+                    var before = trackingEventType.getValue.apply(this, arguments)
+                    var actionInfo = trackingEventType.getActionInfo.apply(this, arguments)                        
+                    var ret = originalDescriptor.set.apply(this, arguments)
+                    var after = trackingEventType.getValue.apply(this, arguments)
+                    addHistoryItem(this, actionInfo.historyKey, {
+                        oldValue: before,
+                        newValue: after,
+                        actionArguments: actionInfo.actionArguments,
+                        actionType: actionInfo.actionType
+                    })
+                    return ret
+                }
+            })
+        }
+        
     })
     
 }
@@ -49,8 +86,10 @@ function addHistoryItem(element, key, data){
     if (!element.__elementHistory[key]) {
         element.__elementHistory[key] = []
     }
+    data.callstack = Error().stack,
+    data.date = new Date()
     element.__elementHistory[key].push(data)
-    console.log("added history")
+    console.log("added history", key, data.actionType)
 }
 
 function disableTracking(){
